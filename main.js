09ba11c039cbb216d1e0097472a8a1b464b01f3c
limanjohnson <126815390+limanjohnson@users.noreply.github.com>
2023-10-16 07:06:21 -0600

    Updated Javascript
    Javascript has been updated so that when a navigation linkn is clicked, it will remain a different color.

--- main.js
+++ main.js
@@ -1,12 +1,24 @@
-// Get the current page's filename (e.g., "about.html")
-const currentPage = window.location.pathname.split("/").pop();
+document.addEventListener("DOMContentLoaded", function () {
+	// JavaScript to mark the active link
+	const currentPage = window.location.pathname.split("/").pop();
+	const navLinks = document.querySelectorAll(".topnav a");
 
-// Get all navigation links
-const navLinks = document.querySelectorAll(".topnav a");
+	navLinks.forEach((link) => {
+		if (link.getAttribute("href") === currentPage) {
+			link.classList.add("active");
+		}
+	});
 
-// Loop through the links and add the "active" class to the current page's link
-navLinks.forEach((link) => {
-	if (link.getAttribute("href") === currentPage) {
-		link.classList.add("active");
-	}
+	// Add a click event listener to the navigation links
+	navLinks.forEach((link) => {
+		link.addEventListener("click", function () {
+			// Remove the "active" class from all links
+			navLinks.forEach((link) => {
+				link.classList.remove("active");
+			});
+
+			// Add the "active" class to the clicked link
+			this.classList.add("active");
+		});
+	});
 });
